c3da20313f807ed9ea0b792fa58ac996e5308ee3
Arun Venkatesh <arunve@infinite.com>
2023-09-07 10:16:48 +0530

    first assignment

---
+++ Training 01/Training 01_2/Programsecond.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training_01_2
+{
+    class Programsecond
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
