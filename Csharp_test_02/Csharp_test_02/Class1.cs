9573a6fb9ad042f970ecf5b467c0ae001eeb5aa4
Arun Venkatesh <arunve@infinite.com>
2023-09-19 10:51:17 +0530

    test02

---
+++ Csharp_test_02/Csharp_test_02/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_test_02
+{
+    public class Product
+
+    {
+
+        public int Productid { get; set; }
+        public string Productname { get; set; }
+        public decimal Price { get; set; }
+
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+
+        {
+            List<Product> prod = new List<Product>();
+            for (int i = 1; i <= 10; i++)            //10 iterations
+            {
+                Product product = new Product();
+                Console.Write($"Enter the details of Product {i}:");
+                Console.Write("Product_iD: ");
+                product.Productid = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Product_name: ");
+                product.Productname = Console.ReadLine();
+                Console.Write("Price: ");
+                product.Price = Convert.ToDecimal(Console.ReadLine());
+                prod.Add(product);
+
+            }
+            prod.Sort((p1, p2) => p1.Price.CompareTo(p2.Price));
+
+            Console.WriteLine("Sorted list of the Products are:"); // list prints in a sorted way
+            foreach (Product product in prod)
+            {
+                Console.WriteLine($"Product_id: {product.Productid}, Product_name: {product.Productname}, Price: {product.Price}");
+                Console.ReadLine();
+            }
+
+        }
+
+    }
+}
---
+++ Csharp_test_02/Csharp_test_02/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Csharp_Test_2
+{
+
+    public abstract class Student
+    {
+        public string Student_Name;
+        public int Student_Id;
+        public double Student_Grade;
+
+
+
+        public abstract bool IsPassed(double grade);
+    }
+    // 2 Child classes 
+    public class Undergraduate : Student
+    {
+        public override bool IsPassed(double grade)
+        {
+            return grade > 70.0;
+        }
+    }
+
+    public class Graduate : Student
+    {
+        public override bool IsPassed(double grade)
+        {
+            return grade > 80.0;
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter details of Undergraduate student:");
+            Undergraduate undergraduateStudent = UndergraduateDetails();
+
+            Console.WriteLine("\nEnter details of Graduate student:");
+            Graduate graduateStudent = GraduateDetails();
+
+            Console.WriteLine("Result for Undergraduate ");
+            Console.WriteLine("\nName: " + undergraduateStudent.Student_Name);
+            Console.WriteLine("Student ID: " + undergraduateStudent.Student_Id);
+            bool UndergraduatePassed = undergraduateStudent.IsPassed(undergraduateStudent.Student_Grade);
+            Console.WriteLine("Passed: " + UndergraduatePassed);
+
+
+            Console.WriteLine("Result ");
+            Console.WriteLine("\nName: " + graduateStudent.Student_Name);
+            Console.WriteLine("Student ID: " + graduateStudent.Student_Id);
+            bool GraduatePassed = graduateStudent.IsPassed(graduateStudent.Student_Grade);
+            Console.WriteLine("Passed: " + GraduatePassed);
+
+
+
+            Console.ReadLine();
+        }
+
+
+
+        public static Undergraduate UndergraduateDetails()
+        {
+            Undergraduate student = new Undergraduate();
+            Console.Write(" Name: ");
+            student.Student_Name = Console.ReadLine();
+            Console.Write(" Student ID: ");
+            student.Student_Id = int.Parse(Console.ReadLine());
+            Console.Write(" Grade: ");
+            student.Student_Grade = double.Parse(Console.ReadLine());
+            return student;
+        }
+
+
+
+        static Graduate GraduateDetails()
+        {
+            Graduate student = new Graduate();
+
+
+
+            Console.Write("Enter Name: ");
+            student.Student_Name = Console.ReadLine();
+
+            Console.Write(" Student ID: ");
+            student.Student_Id = int.Parse(Console.ReadLine());
+
+
+
+            Console.Write(" Grade: ");
+            student.Student_Grade = double.Parse(Console.ReadLine());
+
+
+
+            return student;
+        }
+    }
+}
