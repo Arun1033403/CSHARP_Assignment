c9ec8f36517f613b5e41a4aa171c7d86930ae08e
Arun Venkatesh <arunve@infinite.com>
2023-09-23 10:04:51 +0530

    Assignment_06

---
+++ Assignment_6/Assignment_6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library_Assignment_6;
+
+namespace Assignment_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int TotalFare = 500;
+
+            Concession c = new Concession();
+            Console.WriteLine("Enter your Name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter your Age ");
+            int age = int.Parse(Console.ReadLine());
+            c.CalculateConcession(name, age);
+            Console.Read();
+        }
+    }
+}
---
+++ Assignment_6/ClassLibrary1/class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Assignment_6
+{
+    public class Concession
+
+    {
+        public void CalculateConcession(string name , int age)
+
+        {
+            if (age == 5)
+            {
+
+                Console.WriteLine("Little Champs - Free Ticket");
+            }
+            else if (age >= 60)
+            {
+                double fare = 500 * 0.7; // 30% concession on 500
+
+                Console.WriteLine("Senior Citizen - Calculated Fare: " + fare);
+            }
+            else
+            {
+                Console.WriteLine("Ticket Booked - Fare: 500");
+            }
+        }
+    }
+}
