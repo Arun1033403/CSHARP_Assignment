a86d75b871329d314a69242227c3dcc2f8256199
Arun Venkatesh <arunve@infinite.com>
2023-11-15 20:20:51 +0530

    API_Assignment

--- CRUD_Api/CRUD_Api/Models/Country.cs
+++ CRUD_Api/CRUD_Api/Models/Country.cs
@@ -2,13 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.ComponentModel.DataAnnotations;
 
 namespace CRUD_Api.Models
 {
     public class Country
     {
-        [Key]
         public int ID { get; set; }
         public string CountryName { get; set; }
         public string Capital { get; set; }
